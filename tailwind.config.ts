406d45272cbb3ed0b14f671d1cbca63cd4f6f7a3
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2023-12-07 00:06:29 -0300

    feat: :sparkles: add eslint standard, uuid, swiper.js, color and container styles and main colors
    installing the main dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+		container: {
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem',
+			},
+		},
+		colors: {
+			light: '#F2F2F2',
+
+			gray: '#737373',
+			'gray-soft': '#BFBFBF',
+
+			'dark-soft': '#404040',
+			dark: '#262626',
+
+			primary: '#5E17A6',
+			'primary-dark': '#7A579D',
+
+			secondary: '#F0F235',
+			'secondary-dark': '#B1B354',
+		},
+	},
+	plugins: [],
+};
+export default config;
